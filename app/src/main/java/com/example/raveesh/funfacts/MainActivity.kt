2c63d874b60240d10f46c4a4143236db8be67f52
Raveesh Agarwal <hi@raveeshagarwal.in>
2017-09-28 19:55:35 +0530

    writes all layout in anko.

--- app/src/main/java/com/example/raveesh/funfacts/MainActivity.kt
+++ app/src/main/java/com/example/raveesh/funfacts/MainActivity.kt
@@ -1,29 +1,89 @@
 package com.example.raveesh.funfacts
 
-import android.annotation.TargetApi
+import android.annotation.SuppressLint
+import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
-import android.support.annotation.RequiresApi
 import android.support.v7.app.AppCompatActivity
 import android.view.Window
-import kotlinx.android.synthetic.main.activity_main.*
+import android.widget.RelativeLayout
+import android.widget.TextView
+import org.jetbrains.anko.*
+import org.jetbrains.anko.sdk25.coroutines.onClick
+
 
 class MainActivity : AppCompatActivity() {
 
-    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
-    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+    lateinit var relativeLayout:RelativeLayout
+    lateinit var factTextView: TextView
+    var color = ColorWheel.getColor()
+    lateinit var fact:String
+
+
+
+    @SuppressLint("ResourceType", "SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        showFactButton.setOnClickListener {
-            factTextView.text = FactBook.getFact()
-            // Colors:
-            val color = ColorWheel.getColor()
-            relativeLayout.setBackgroundColor(color)
-            showFactButton.setTextColor(color)
-            val window:Window = this.window
+        val window: Window = this.window
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             window.statusBarColor = color
         }
+
+
+        relativeLayout = relativeLayout {
+            padding = dip(30)
+            backgroundColor = color
+
+            textView {
+                text = "Did You Know?"
+                textColor = Color.parseColor("#DDFFFFFF")
+                textSize = 30f
+            }.lparams {
+                topMargin = dip(10)
+                width = wrapContent
+                height = wrapContent
+                alignParentStart()
+                alignParentTop()
+            }
+
+            factTextView = textView {
+                text = "This app shows facts when you click the button!"
+                textSize = 25f
+                textColor = Color.WHITE
+            }.lparams {
+                above(2)
+                width = wrapContent
+                height = wrapContent
+            }
+
+            button("Show More!") {
+                id = 2
+                textSize = 15f
+                backgroundColor = Color.WHITE
+                onClick {
+                    color = ColorWheel.getColor()
+                    fact = FactBook.getFact()
+                    textColor = color
+                    clicked(color, fact) }
+            }.lparams {
+                width = matchParent
+                alignParentBottom()
+                bottomMargin = dip(30)
+                topMargin = dip(25)
+            }
+        }
     }
+
+    private fun clicked(color:Int, text:String) {
+        factTextView.text = text
+        relativeLayout.backgroundColor = color
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            window.statusBarColor = color
+        }
+
+
+    }
+
+
 }
